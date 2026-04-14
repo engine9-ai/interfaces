f1644d6b5d7efbcec9f049681a55b1fc537cee45
Chris Lundberg <chris@frakture.com>
2026-04-14 00:53:17 +0000

    Confirm input_id

--- timeline/transforms/inbound/upsert_tables.js
+++ timeline/transforms/inbound/upsert_tables.js
@@ -1,14 +1,14 @@
 export const bindings = {
   tablesToUpsert: { path: 'sql.tables.upsert' }
 };
-export async function transform({ batch, tablesToUpsert, inputId }) {
+export async function transform({ batch, tablesToUpsert }) {
   tablesToUpsert.timeline = tablesToUpsert.timeline || [];
   batch.forEach((o) => {
     if (o.timeline_id) {
       tablesToUpsert.timeline.push({
         id: o.timeline_id,
         ts: o.ts || new Date(),
-        input_id: inputId,
+        input_id: o.input_id,
         entry_type_id: o.entry_type_id,
         person_id: o.person_id,
         source_code_id: o.source_code_id
