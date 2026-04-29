b7465d9c7a4ccf5391bbd3188e98c334bfc3e171
Chris Lundberg <chris@frakture.com>
2026-04-29 16:39:25 +0000

    Transactions

--- transaction/transforms/inbound/upsert_tables.js
+++ transaction/transforms/inbound/upsert_tables.js
@@ -1,8 +1,12 @@
 import { getEntryTypeId, getTimelineEntryUUID } from '@engine9/input-tools';
-/* appends a recurring int if recurs is specified */
+/* appends a recurs_id no matter what */
 function appendRecursId(d) {
-  if (d.recurs_id !== undefined) return d;
-  if (!d.recurs) return d;
+  //If there's a recurs_id that's positive, use it.
+  if (parseInt(d.recurs_id)) {
+    d.recurs_id = parseInt(d.recurs_id);
+    return d;
+  }
+  //Otherwise set it based on the 'recurs' flag
   switch (d.recurs) {
     case 'semi-annually':
       d.recurs_id = 6;
@@ -37,11 +41,11 @@ export async function transform(opts) {
   if (batch.length === 0) return;
   tablesToUpsert.transaction = tablesToUpsert.transaction || [];
   const hasRecurs = batch.some((b) => b.recurs !== undefined || b.recurs_id !== undefined);
-  if (hasRecurs) {
-    batch.forEach((b) => (b.recurs_id = b.recurs_id || 0));
-  }
+
   batch.forEach((o) => {
-    appendRecursId(o);
+    if (hasRecurs) {
+      appendRecursId(o);
+    }
     o.entry_type_id = getEntryTypeId(o);
     o.id = getTimelineEntryUUID(o, { defaults: { plugin_id: pluginId } });
     tablesToUpsert.transaction.push(o);
