03c2333cfd97ab1ed42ac4da15343f35b1ecb513
Chris Lundberg <chris@frakture.com>
2026-09-16 18:06:14 +0000

    Add support for hidden settings

--- plugin/settings.js
+++ plugin/settings.js
@@ -3,6 +3,7 @@ export const settings = [
     name: 'table_prefix_counter',
     type: 'int',
     default: 2729,
+    hidden: true,
     description: 'Hex table-prefix allocator. Incremented by PluginWorker.getNextTablePrefixCounter.'
   }
 ];
