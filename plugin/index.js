8422bf24cfc9fb239a1a6572301781b111526225
Chris Lundberg <chris@frakture.com>
2026-09-16 15:54:58 +0000

    Settings for prefixing

--- plugin/index.js
+++ plugin/index.js
@@ -1,11 +1,14 @@
 import schema from './schema.js';
+import { settings } from './settings.js';
 const metadata = {
   name: '@engine9/interfaces/plugin',
   version: '1.0.0'
 };
 export { metadata };
 export { schema };
+export { settings };
 export default {
   metadata,
-  schema
+  schema,
+  settings
 };
--- plugin/settings.js
+++ plugin/settings.js
@@ -1,7 +1,9 @@
 export const settings = [
   {
     name: 'table_prefix_counter',
-    type: 'int'
+    type: 'int',
+    default: 2729,
+    description: 'Hex table-prefix allocator. Incremented by PluginWorker.getNextTablePrefixCounter.'
   }
 ];
 export default {
